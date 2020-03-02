59097e21b4e9cb444658497e4d63d09bac3c34ea
ElMatador26 <53703824+ElMatador26@users.noreply.github.com>
2020-03-02 19:40:07 +0530

    Add files via upload

--- kruskals.c
+++ kruskals.c
@@ -1,87 +1,72 @@
 #include<stdio.h>
 #include<stdlib.h>
+//updated
 typedef struct node
 {
-	int wt, i, j;
+	int i, j, wt;
 	struct node *link;
 }node;
-
 typedef struct set
 {
-	node *rear, *front;
+	node *front, *rear;
 	int s;
 }set;
 void kruskals(node *head, int n)
 {
-	set *arr=(set *)calloc(n, sizeof(set));
-	int s=0;
-	node *head2;
-	while(head != NULL)
+	set *arr;
+	node *head2=NULL;
+	arr=(set *)calloc(n, sizeof(set));
+	printf("s=%d\n", arr[head->i].s);
+	while(head!=NULL)
 	{
-		head2=head;
-		if(arr[head->i].front != arr[head->j].front || arr[head->i].s == 0)	
-		{
-			if(arr[head->j].front != NULL)
-			{
-				arr[head->j].rear=(arr[head->j].rear)->link=head;
-			}
+		if(arr[head->i].s == 0 )
+			printf("Hi");
+		if(arr[head->i].s == 0 )//|| arr[head->i].front != arr[head->j].front)
+		{	
+			printf("Hello");
+			if(arr[head->i].front == NULL)
+				arr[head->i].front=arr[head->i].rear=head;
 			else
-				arr[head->j].front=arr[head->j].rear=head;
-			(arr[head->j].s)++;
-			(arr[head->i].rear)->link=arr[head->j].front;
-			arr[head->j].front=arr[head->i].front;
-			arr[head->j].s=arr[head->j].s+arr[head->i].s;
-			while(arr[head->i].front != NULL)
+				arr[head->i].rear=(arr[head->i].rear)->link=head;
+			arr[head->i].rear->link=arr[head->j].front;
+			arr[head->i].rear=arr[head->j].rear;
+			arr[head->i].s=1+arr[head->i].s+arr[head->j].s;
+			head2=arr[head->i].front;
+			while(1)
 			{
-				arr[(arr[head->i].front)->j]=arr[(arr[head->i].front)->i]=arr[head->j];
-				arr[head->i].front=(arr[head->i].front)->link;
+				arr[head2->i]=arr[head2->j]=arr[head->i];
+				if(head2 == arr[head->i].rear)
+					break;
+				head2=head2->link;
 			}
-			arr[head->i]=arr[head->j];
-			head=head->link;
-			head2->link=NULL;
 		}
-		else
-		{
-			head=head->link;
-			free(head2);
-		}
+		head=head->link;
 	}
-	if(arr[0].s == n-1)
-	{
-		head2=arr[0].front;
-		while(head2 != NULL)
-		{
-			printf("i=%d j=%d wt=%d\n", head2->i+1, head2->j+1, head2->wt);
-			s=s+head2->wt;
-			head2=head2->link;
-		}
-	}
+	if(arr[0].s == n)
+		printf("Spanning tree is formed");
 	else
-		printf("Multiple spanning forests are present\n");
-	}
-
+		printf("Spanning forests detected");
+}
 int main()
 {
 	node *head=NULL, *head2;
-	int i=1, s=0;
+	int i=1, s=-1;
 	while(i != 0)
 	{
-		head2=(node *)calloc(1, sizeof(node));
-		printf("Enter source vertex: ");
-		scanf("%d", &(head2->i));
-		printf("Enter destination vertex: ");
-		scanf("%d", &(head2->j));
-		printf("Enter the weight: ");
-		scanf("%d", &(head2->wt));
+		head2=(node *)malloc(sizeof(node));
 		head2->link=head;
 		head=head2;
-		printf("Enter 0 to exit: ");
+		printf("Enter the initial vertex: ");
+		scanf("%d", &(head->i));
+		printf("Enter the end vertex: ");
+		scanf("%d", &(head->j));
+		printf("Enter the weight: ");
+		scanf("%d", &(head->wt));
+		printf("Enter 0 to stop: ");
 		scanf("%d", &i);
+		s=(s >= head->i)?s:head->i;
+		s=(s >= head->j)?s:head->j;
 	}
-	kruskals(head, 5);
+	kruskals(head, s+1);
 	return 0;
-}
-			
-
-
-
+}			
