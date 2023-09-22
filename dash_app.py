455affb48c3fa808c9b7589345a03c10ebfd4921
ElMatador26 <53703824+ElMatador26@users.noreply.github.com>
2023-09-22 23:34:33 +0530

    Add files via upload

--- dash_app.py
+++ dash_app.py
@@ -1,4 +1,179 @@
-from mst import data_processing
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  8 15:54:03 2023
+
+@author: 2130006
+"""
+
+
+# from data import data_processing
+# from dash import Dash, html, dcc, Input, Output, callback, State, ctx, dash_table
+# import pandas as pd
+# import datetime
+# data = data_processing()
+# time = None
+# fig = data.create_empty_graph()
+# graph = dcc.Graph(figure=fig, id='graph')
+# input_min = dcc.Input(id='min', type='number', min=2000, max=2030, value=2018)
+# input_max = dcc.Input(id='max', type='number', min=2000, max=2030, value=2030)
+# submit = html.Button(id='submit', children='Submit', n_clicks=0)
+# side_bar = [html.H1('Input min year'), input_min,
+#             html.H1('Input max year'), input_max, submit]
+# upload_button = html.Button(id='upload', children='Upload', n_clicks=0)
+# filter_div = html.Div(side_bar, id='sidebar', style={'visibility': 'hidden'})
+# name = dcc.Input(id='name')
+# dept = dcc.Input(id='dept')
+# salary = dcc.Input(id='salary', type='number')
+# button = html.Button(id='button', children='Submit', n_clicks=0)
+# click_div = html.Div(children=[name, dept, salary, button], style={
+#                      'display': 'none'}, id='answer')
+# col1 = html.Div(id='col1', children=[upload_button, filter_div, click_div])
+# col2_style = {
+#     'height': '70%',
+#     'width': '100%',
+#     'padding-right': '1em',
+#     'border-left': '0.4em solid',
+#     'border-color': 'deepskyblue',
+#     'padding-left': '1em',
+#     'padding-top': '2em',
+#     'padding-right': '1em'
+# }
+
+# tab1 = dcc.Tab(label='Graph View', value='gv')
+# tab2 = dcc.Tab(label='Dataframe View', value='dv')
+# tab = dcc.Tabs(id='tab', children=[tab1, tab2], value='gv')
+# div1 = html.Div(id='tab_gv_output', children=graph)
+# div2 = html.Div(id='tab_dv_output', style={
+#                 'display': 'none'}, children = 'No data')
+# col2 = html.Div(id='col2', children=[tab, div1, div2], style=col2_style)
+# flex_div = html.Div(children=[col1, col2], id='flex', style={'display': 'flex',
+#                                                              'flex-direction': 'row'})
+
+# main_div = [flex_div, dcc.Store(id = 'max_val'), dcc.Store(id = 'min_val'), dcc.Store(id = 'timestamp')] 
+
+# # tabs callback
+# @callback(Output(component_id='tab_gv_output', component_property='style'),
+#           Output(component_id='tab_dv_output', component_property='style'),
+#           Input(component_id='tab', component_property='value'),
+#           prevent_initial_call=True)
+# def tab_switch(tab):
+#     if tab == 'gv':
+#         return [{}, {'display': 'none'}]
+#     if tab == 'dv':
+#         if data.fig is None:
+#             return [{'display': 'none'}, {}]
+#         else:
+            
+#             return [{'display': 'none'}, {}]
+
+# # upload callback
+# @callback(Output(component_id='graph', component_property='figure', allow_duplicate=True),
+#           Output(component_id='sidebar', component_property='style'),
+#           Output(component_id='tab_dv_output',
+#                  component_property='children', allow_duplicate=True),
+#           Output('timestamp', 'data', allow_duplicate=True),
+#           Input(component_id='upload', component_property='n_clicks'),
+#           State('timestamp', 'data'),
+#           prevent_initial_call=True)
+# def update(upload, val):
+#     trig_id = ctx.triggered_id
+#     global time
+#     if data.df is None:
+#         val = time = datetime.datetime.now()
+    
+#     if trig_id == 'upload':
+#         fig = data.create_mst_graph()
+#         arr = []
+#         df = data.df.drop(columns=['x', 'y'])
+#         for i in df.columns:
+#                 arr.append({"name": i, "id": i})
+#         dataframe = dash_table.DataTable(df.to_dict('records'), arr)
+#         return [fig, {}, dataframe, val]
+    
+# # edit df callback
+# @callback(Output(component_id='graph', component_property='figure', allow_duplicate=True),
+#           Input('button', 'n_clicks'),
+#           State('dept', 'value'),
+#           State('salary', 'value'),
+#           State('name', 'value'),
+#           prevent_initial_call=True)
+# def update_data(button, dept, salary, name):
+#     global time
+#     graph = data.update_df(name, dept, salary)
+#     time = datetime.datetime.now()
+#     return graph
+
+# # make edit inputs disappear
+# @callback(Output(component_id='answer', component_property='style', allow_duplicate=True),
+#           Input('button', 'n_clicks'),
+#           prevent_initial_call=True)
+# def hide(button):
+#     return {'display': 'none'}
+
+# # filter callback
+# @callback(Output(component_id='graph', component_property='figure', allow_duplicate=True),
+#           Output('min_val', 'data'),
+#           Output('max_val', 'data'),
+#           Output(component_id='tab_dv_output', component_property='children', allow_duplicate=True),
+#           Input(component_id='submit', component_property='n_clicks'),
+#           State(component_id='min', component_property='value'),
+#           State(component_id='max', component_property='value'),
+#           prevent_initial_call = True) 
+# def filter_graph(submit, min_val, max_val):
+#     filter_df, fig = data.update_graph([min_val, max_val])
+#     arr = []
+#     df = filter_df.drop(columns=['x', 'y'])
+#     for i in df.columns:
+#         arr.append({"name": i, "id": i})
+#     dataframe = dash_table.DataTable(df.to_dict('records'), arr)
+#     return [fig, min_val, max_val, dataframe]
+
+
+
+    
+# # clickdata callback
+# @callback(Output(component_id='answer', component_property='style'),
+#           Output('name', 'value'),
+#           Output('name', 'readOnly'),
+#           Output('dept', 'value'),
+#           Output('salary', 'value'),
+#           Input(component_id='graph', component_property='clickData'),
+#           prevent_initial_call=True)
+# def update(clickData):
+#     if clickData is not None:
+
+#         name = clickData['points'][0]['customdata'][0]
+#         dept = clickData['points'][0]['customdata'][1]
+#         salary = clickData['points'][0]['customdata'][3]
+
+#         return [{'padding-top': '2em'}, name, True, dept, salary]
+#     else:
+#         return [{'display': 'none'}, '', False, '', '']
+
+
+
+
+
+
+# div_style = {
+#     'opacity': '0.8',
+#     'background-color': '#ccc',
+#     'position': 'fixed',
+#     'width': '100%',
+#     'height': '100%',
+#     'top': '0px',
+#     'left': '0px',
+#     'z-index': '1000'
+# }
+
+
+# app = Dash(__name__, suppress_callback_exceptions=True)
+# app.layout = html.Div(main_div, style=div_style)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+from data import data_processing
 from dash import Dash, html, dcc, Input, Output, callback, State, ctx, dash_table
 import pandas as pd
 import datetime
@@ -41,9 +216,8 @@ col2 = html.Div(id='col2', children=[tab, div1, div2], style=col2_style)
 flex_div = html.Div(children=[col1, col2], id='flex', style={'display': 'flex',
                                                              'flex-direction': 'row'})
 
-main_div = [flex_div, dcc.Store(id = 'max_val'), dcc.Store(id = 'min_val'), dcc.Store(id = 'timestamp'),
-            dcc.Interval(id = 'interval', interval=1000, n_intervals=0)] 
-
+main_div = [flex_div, dcc.Store(id = 'max_val'), dcc.Store(id = 'min_val'), dcc.Store(id = 'timestamp')] 
+# main_div.append(dcc.Interval(id = 'interval', interval=5*1000, n_intervals=0))
 # tabs callback
 @callback(Output(component_id='tab_gv_output', component_property='style'),
           Output(component_id='tab_dv_output', component_property='style'),
@@ -67,14 +241,17 @@ def tab_switch(tab):
           Output('timestamp', 'data', allow_duplicate=True),
           Input(component_id='upload', component_property='n_clicks'),
           State('timestamp', 'data'),
+          State('graph', 'figure'),
+          State('tab_dv_output', 'children'),
           prevent_initial_call=True)
-def update(upload, val):
+def update(upload, val, fig, dataframe):
     trig_id = ctx.triggered_id
     global time
-    if data.df is None:
+    if time is None:
         val = time = datetime.datetime.now()
-    
-    if trig_id == 'upload':
+    else:
+        val = time
+    if upload == 0:
         fig = data.create_mst_graph()
         arr = []
         df = data.df.drop(columns=['x', 'y'])
@@ -82,19 +259,34 @@ def update(upload, val):
                 arr.append({"name": i, "id": i})
         dataframe = dash_table.DataTable(df.to_dict('records'), arr)
         return [fig, {}, dataframe, val]
+    else:
+        return [fig, {}, dataframe, val]
     
 # edit df callback
 @callback(Output(component_id='graph', component_property='figure', allow_duplicate=True),
+          Output(component_id='tab_dv_output', component_property='children', allow_duplicate=True),
           Input('button', 'n_clicks'),
           State('dept', 'value'),
           State('salary', 'value'),
           State('name', 'value'),
+          State('min_val', 'data'),
+          State('max_val', 'data'),
           prevent_initial_call=True)
-def update_data(button, dept, salary, name):
+def update_data(button, dept, salary, name, min_val, max_val):
     global time
+    
     graph = data.update_df(name, dept, salary)
+    df = data.df.drop(columns=['x', 'y'])
+    if min_val is not None and max_val is not None:
+        df, graph = data.update_graph([min_val, max_val])
+        df = df.drop(columns=['x', 'y'])
+    
+    arr = []
+    for i in df.columns:
+            arr.append({"name": i, "id": i})
+    dataframe = dash_table.DataTable(df.to_dict('records'), arr)
     time = datetime.datetime.now()
-    return graph
+    return [graph, dataframe]
 
 # make edit inputs disappear
 @callback(Output(component_id='answer', component_property='style', allow_duplicate=True),
@@ -145,29 +337,28 @@ def update(clickData):
 
 
 # update graph and dataframe
-@callback(Output(component_id='graph', component_property='figure'),
-          Output(component_id='tab_dv_output', component_property='children'),
-          Output('timestamp', 'data'),
-          Input(component_id='interval', component_property='n_intervals'),
-          State('min_val', component_property='data'),
-          State('max_val', component_property='data'),
-          State('timestamp', 'data'),
-          State(component_id='graph', component_property='figure'),
-          State(component_id='tab_dv_output', component_property='children'),
-          prevent_initial_call = True)
-def interval_update(n_interval, min_val, max_val, timestamp, graph, dv):
-    global time
-    print(time, timestamp)
-    if time > timestamp:
-        df, fig = data.update_graph([min_val, max_val])
-        arr = []
-        df = data.df.drop(columns=['x', 'y'])
-        for i in df.columns:
-                arr.append({"name": i, "id": i})
-        dataframe = dash_table.DataTable(df.to_dict('records'), arr)
-        return [fig, dataframe, datetime.datetime.now()]
-    else:
-        return [graph, dv, timestamp]
+# @callback(Output(component_id='graph', component_property='figure'),
+#           Output(component_id='tab_dv_output', component_property='children'),
+#           Output('timestamp', 'data'),
+#           Input(component_id='interval', component_property='n_intervals'),
+#           State('min_val', component_property='data'),
+#           State('max_val', component_property='data'),
+#           State('timestamp', 'data'),
+#           State(component_id='graph', component_property='figure'),
+#           State(component_id='tab_dv_output', component_property='children'),
+#           prevent_initial_call = True)
+# def interval_update(n_interval, min_val, max_val, timestamp, graph, dv):
+#     global time
+#     if time is not None and time > timestamp:
+#         df, fig = data.update_graph([min_val, max_val])
+#         arr = []
+#         df = data.df.drop(columns=['x', 'y'])
+#         for i in df.columns:
+#                 arr.append({"name": i, "id": i})
+#         dataframe = dash_table.DataTable(df.to_dict('records'), arr)
+#         return [fig, dataframe, datetime.datetime.now()]
+#     else:
+#         return [graph, dv, timestamp]
 
 
 
---
+++ data.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 20 10:15:40 2023
+
+@author: 2130006
+"""
+
+import pandas as pd
+import nltk
+import numpy as np
+from scipy import sparse
+from scipy.sparse.csgraph import minimum_spanning_tree
+import plotly.express as px
+
+
+class data_processing:
+    # this class be used to work with data
+
+    def __init__(self):
+        # this method creates dummy data when there is no dataframe
+
+        dict1 = {'name': ['Ayan', 'Ram', 'Shyam', 'Jodu', 'Modu'],
+                 'department': ['', '', '', '', ''],
+                 'doj': ['10/2/2021', '4/23/2023', '8/8/2021', '2/15/2019', '2/2/2020'],
+                 'salary': [1000, 200, 300, 500, 600],
+                 'edited': [False, False, False, False, False]}
+
+        self.df = pd.DataFrame(dict1)
+        self.df['doj'] = pd.to_datetime(self.df.doj, format='%m/%d/%Y')
+        self.mat = None
+        # self.filter_df = None
+        self.fig = None
+
+    def _init_(self, df):
+        # this method accepts the dataframe if its given
+        self.df = df
+        self.mat = None
+        # self.filter_df = None
+        self.fig = None
+
+    def distance(self, col: str = 'name'):
+        # calculates the distance when given an attribute
+
+        mat = np.zeros(shape=(self.df.shape[0], self.df.shape[0]))
+        for i in range(self.df.shape[0]):
+            for j in range(i, self.df.shape[0]):
+                mat[i, j] = (nltk.edit_distance(
+                    self.df.loc[i, col], self.df.loc[j, col]))
+        mat = sparse.csr_matrix(mat)
+        return mat
+
+    def __coord_generator(self, prev=None, d=None):
+
+        # make generate coordinates of new point based on old point
+
+        if prev is None:
+            x = np.random.uniform(0, 10)
+            y = np.random.uniform(0, 10)
+        else:
+            angle = np.random.uniform(0, 7)
+            x = prev[0]+(d*np.sin(angle))
+            y = prev[1]+(d*np.cos(angle))
+        
+        return (x, y)
+
+    def mst(self):
+        #find the minimum spanning tree
+
+        mat = self.mat
+        if mat is None:
+            mat = self.distance()
+        mat = self.mat = minimum_spanning_tree(mat)
+        return mat
+
+    def __create_coord_dict(self, mat):
+        #creates the coordinates of the data
+
+        l = mat.shape[0]
+        mat = sparse.coo_matrix(mat)
+        dict1 = None
+        i = 0
+        j = 0
+        while l != j:
+            if dict1 is None:
+                dict1 = {}
+                dict1[mat.row[i]] = self.__coord_generator()
+                j += 1
+            if mat.row[i] in dict1 and mat.col[i] not in dict1:
+                dict1[mat.col[i]] = self.__coord_generator(
+                    dict1[mat.row[i]], mat.data[i])
+                j += 1
+            if mat.row[i] not in dict1 and mat.col[i] in dict1:
+                dict1[mat.row[i]] = self.__coord_generator(
+                    dict1[mat.col[i]], mat.data[i])
+                j += 1
+
+            i = (i+1) % l
+        return dict1
+
+    def create_empty_graph(self):
+        # creates the empty graph
+
+        fig = px.scatter()
+        fig.update_yaxes(tickmode='linear', tick0=0, dtick=1)
+        fig.update_xaxes(tickmode='linear', tick0=0, dtick=1)
+        fig.update_layout(title_text='Sample Dataset')
+        return fig
+
+    def create_graph(self, df):
+        # create a graph from a dataframe
+        
+        if self.fig is None:
+            hover_dict = {}
+            for i in self.df.columns:
+                if i == 'x' or i == 'y' or i == 'edited':
+                    hover_dict[i] = False
+                else:
+                    hover_dict[i] = True
+            
+            fig = px.scatter(self.df, x='x', y='y', size='salary',
+                         hover_data=hover_dict)
+            fig.update_yaxes(tickmode='linear', tick0=0, dtick=1)
+            fig.update_xaxes(tickmode='linear', tick0=0, dtick=1)
+            fig.update_layout(title_text='Sample Dataset')
+            fig.update_traces(marker=dict(color='green'))
+            self.fig = fig
+        
+        else:
+            fig = self.fig
+            fig.data[0].x = df['x']
+            fig.data[0].y = df['y']
+            fig.data[0].marker.size = df['salary']
+            fig.data[0].customdata = df
+            arr = ['blue' if i else 'green' for i in df['edited']]
+            fig.update_traces(marker=dict(color=arr))
+        return fig
+
+
+    def create_mst_graph(self):
+        # creates the mst graph in plotly
+        
+        if self.fig is not None:
+            return self.fig
+        mat = self.mst()
+        dict1 = self.__create_coord_dict(mat)
+        for i in dict1:
+            self.df.loc[i, 'x'] = dict1[i][0]
+            self.df.loc[i, 'y'] = dict1[i][1]
+
+        return self.create_graph(self.df)
+
+    def update_graph(self, arr):
+        # updates the graph based on the condition
+
+        df = self.df[(self.df['doj'].dt.year >= arr[0])
+                     & (self.df['doj'].dt.year <= arr[1])]
+        
+        filter_df = df.reset_index(drop=True)
+        return filter_df, self.create_graph(filter_df) 
+        # return self.create_graph(self.filter_df)
+
+    def update_df(self, name, dept, salary):
+        index = 0
+        for i in self.df['name']:
+            if i == name:
+                break
+            index += 1
+        self.df.loc[index, 'salary'] = salary
+        self.df.loc[index, 'department'] = dept
+        self.df.loc[index, 'edited'] = True
+        df = self.df
+
+        # if self.filter_df is not None:
+
+        #     index = 0
+        #     for i in self.filter_df['name']:
+        #         if i == name:
+        #             break
+        #         index += 1
+        #     self.filter_df.loc[index, 'salary'] = salary
+        #     self.filter_df.loc[index, 'department'] = dept
+        #     self.filter_df.loc[index, 'edited'] = True
+        #     df = self.filter_df
+        
+        return self.create_graph(df)
